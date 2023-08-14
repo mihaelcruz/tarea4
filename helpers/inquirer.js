026f8d77d6887ad7251c4bdc7ed583f93f4d081d
mihael <mihael.cruz@gmail.com>
2023-08-14 07:15:08 -0400

    act2

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -5,7 +5,7 @@ const preguntas=[
 {
     type: 'list',
     name: 'opcion',
-    message: '¿Qué desea hacer?', 
+    message: '¿Qué desea hacer?',
     choices: [
         {
             value:'1',
@@ -16,22 +16,6 @@ const preguntas=[
             name:'2. Listar tarea'
         },
         {
-            value:'3',
-            name:'3. Listar tareas completadas'
-        },
-        {
-            value:'4',
-            name:'4. Listar tareas pendientes'
-        },
-        {
-            value:'5',
-            name:'5. Completar tareas'
-        },
-        {
-            value:'6',
-            name:'6. Borrar tareas'
-        },
-        {
             value:'0',
             name:'0. Salir'
         },
@@ -40,10 +24,10 @@ const preguntas=[
 ];
 
 const inquirerMenu = async()=>{
-    console.clear(); 
+    console.clear();
     console.log("=======================".green);
-    console.log("Seleccione un Opcion".green); 
-    console.log("=======================\n".green); 
+    console.log("Seleccione un Opcion".green);
+    console.log("=======================\n".green);
     const {opcion} = await inquirer.prompt (preguntas);
     return opcion;
  }
@@ -55,12 +39,10 @@ const inquirerMenu = async()=>{
             name:'enter',
             message:`Presione ${'enter'.green} para continuar`
         }
-    ];
-    await inquirer.prompt(question);
+    ]
  }
 
 
  module.exports={
-    inquirerMenu,
-    pausa
+    inquirerMenu
  }
